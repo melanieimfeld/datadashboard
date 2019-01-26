937325be2d0f31f8f045082a9de68d27756db188
Melanie Imfeld <melanieimfeld@Melanies-MacBook-Pro.local>
2019-01-26 20:28:51 +0100

    updates

--- main.py
+++ main.py
@@ -1,24 +1,47 @@
-#import json
+import json
+import scraper
+import sys
 
-from flask import Flask, render_template
-#import pandas as pd
+from flask import Flask, render_template, request
+import pandas as pd
+
+import requests
 
 app = Flask(__name__)
 
-# @app.route("/")
+@app.route("/")
 # def index():
 #     df = pd.read_csv('data.csv').drop('Open', axis=1)
 #     chart_data = df.to_dict(orient='records')
 #     chart_data = json.dumps(chart_data, indent=2)
 #     data = {'chart_data': chart_data}
 #     return render_template("form.html", data=data)
-@app.route('/')
+
+#@app.route('/')
 def hello_world():
-    return 'Hello, World!'
+	#test = scraper.scrape()
+	return render_template('result.html')
+
+
+@app.route('/hello')
+def hello():
+    return 'Hello, another World'
+
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+	if request.method == 'POST':
+		query = request.form.to_dict() 
+		query = query.get('Name') #get keyword user entered
+		output = scraper.scrape(query) #call scraper
+		return output
+		
+#return render_template("result.html",result = result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
 
 #sources:
+#https://medium.com/@wahyudihandry/how-to-build-web-scraping-using-beautifulsoup-and-flask-part-i-ca38a167c236
 #https://medium.freecodecamp.org/how-to-build-a-web-application-using-flask-and-deploy-it-to-the-cloud-3551c985e492
 #https://cloud.google.com/appengine/docs/standard/python/getting-started/python-standard-env
